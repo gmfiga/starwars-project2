d5180fc474f5f7ab348e15236f79d91a65711ecb
gmfiga <gmfiga@gmail.com>
2023-07-05 15:42:34 -0400

    fixed styles

--- starwars-project2/src/CharacterPage.js
+++ starwars-project2/src/CharacterPage.js
@@ -1,55 +1,66 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const CharacterPage = () => {
+  const [character, setCharacter] = useState({});
+  const [planet, setPlanet] = useState({});
+  const [films, setFilms] = useState([]);
+  let params = useParams();
 
-    const [character, setCharacter] = useState({});
-    const [planet, setPlanet] = useState({});
-    const [films, setFilms] = useState([]);
-    let params = useParams();
+  useEffect(() => {
+    //fetch
+    fetch(`http://localhost:4000/api/characters/${params.id}`)
+      .then((res) => res.json())
+      .then((char) => setCharacter(char))
+      .then(
+        fetch(`http://localhost:4000/api/characters/${params.id}/films`)
+          .then((res) => res.json())
+          .then((films) => setFilms(films))
+      );
+  }, [params]);
 
-    useEffect(() => { 
-        //fetch
-        fetch(`http://localhost:4000/api/characters/${params.id}`)
-        .then(res => res.json())
-        .then(char => setCharacter(char)).then(
-            fetch(`http://localhost:4000/api/characters/${params.id}/films`)
-            .then(res => res.json())
-            .then(films => setFilms(films))
-        );
-    }, [params]);
-
-    useEffect(() => { 
-        if (character.homeworld) {
-            fetch(`http://localhost:4000/api/planets/${character.homeworld}`)
-            .then(res => res.json()) 
-            .then(planet => setPlanet(planet));
-        }
-    }, [character]);
+  useEffect(() => {
+    if (character.homeworld) {
+      fetch(`http://localhost:4000/api/planets/${character.homeworld}`)
+        .then((res) => res.json())
+        .then((planet) => setPlanet(planet));
+    }
+  }, [character]);
 
   return (
     <>
-        <h1 id="name">{character.name}</h1>
-        <section id="generalInfo">
-        <p>Height: <span id="height"></span>{character.height} cm</p>
-        <p>Mass: <span id="mass"></span>{character.mass} kg</p>
-        <p>Born: <span id="birth_year"></span>{character.birth_year}</p>
-        </section>
-        <section id="planets">
+      <h1 id="name">{character.name}</h1>
+      <section id="generalInfo">
+        <p>
+          Height: <span id="height"></span>
+          {character.height} cm
+        </p>
+        <p>
+          Mass: <span id="mass"></span>
+          {character.mass} kg
+        </p>
+        <p>
+          Born: <span id="birth_year"></span>
+          {character.birth_year}
+        </p>
+      </section>
+      <section id="planets">
         <h2>Homeworld</h2>
-            <p><span id="homeworld">{planet.name}</span></p>
-        </section>
-        <section id="films">
-            <h2>Films appeared in</h2>
-            <ul>
-                {films.map(
-                    (film) => {return <li key={film.id}>{film.title}</li>}
-                )}
-            </ul>
-        </section>
+        <p>
+          <li id="homeworld">{planet.name}</li>
+        </p>
+      </section>
+      <section id="films">
+        <h2>Films appeared in</h2>
+        <ul>
+          {films.map((film) => {
+            return <li key={film.id}>{film.title}</li>;
+          })}
+        </ul>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default CharacterPage
+export default CharacterPage;
